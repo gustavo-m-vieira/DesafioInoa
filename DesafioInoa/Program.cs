9d89c70cee915c6f434c461802238ad4dbf76e3c
gustavo-m-vieira <gustavo.vieira@poli.ufrj.br>
2022-05-13 10:06:47 -0300

    reading input

--- DesafioInoa/Program.cs
+++ DesafioInoa/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace DesafioInoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+            (string ticker, float sellPrice, float buyPrice) = parseArgs(args);
+
+            Console.WriteLine(ticker);
+            Console.WriteLine(sellPrice);
+            Console.WriteLine(buyPrice);
+        }
+
+        static Tuple<string, float, float> parseArgs(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                throw new ArgumentException("You must pass ticker, price to sell and price to buy!");
+            }
+
+            string ticker = args[0];
+            float sellPrice;
+            float buyPrice;
+
+            if (!float.TryParse(args[1], out sellPrice)) throw new ArgumentException("sellPrice must be a float value!");
+            if (!float.TryParse(args[2], out buyPrice)) throw new ArgumentException("buyPrice must be a float value!");
+
+            return Tuple.Create(ticker, sellPrice, buyPrice);
+        }
+
+    }
+}
