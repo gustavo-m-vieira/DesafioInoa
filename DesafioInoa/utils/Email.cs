fc38a67ae55f0cfc3b07d33f979661217503d49e
gustavo-m-vieira <gustavo.vieira@poli.ufrj.br>
2022-05-16 13:58:50 -0300

    Starting to handle email

---
+++ DesafioInoa/utils/Email.cs
@@ -0,0 +1,45 @@
+﻿using System.Configuration;
+
+namespace DesafioInoa.utils
+{
+	public class EmailHandler
+	{
+		
+		private float sellPrice;
+		private float buyPrice;
+		private string ticker;
+
+		public EmailHandler(string ticker, float sellPrice, float buyPrice)
+		{
+			var settings = ConfigurationManager.AppSettings;
+
+			this.sellPrice = sellPrice;
+			this.buyPrice = buyPrice;
+			// this.emailToNotify = settings["to"];
+			// this.emailFrom = settings["from"];
+			// this.emailPassword = settings["password"];
+		}
+
+		private void sendMail(string subject, string message)
+        {
+
+        }
+
+		public void notifyToSell(float currentPrice)
+        {
+			string subject = "Alert! Time to sell!";
+			string message = "The ticker" + this.ticker + "has riched the selling point " + this.sellPrice + "! It's price is " + currentPrice;
+
+			sendMail(subject, message);
+        }
+
+		public void notifyToBuy(float currentPrice)
+        {
+			string subject = "Alert! Time to buy!";
+			string message = "The ticker" + this.ticker + "has riched the buying point " + this.buyPrice + "! It's price is " + currentPrice;
+
+			sendMail(subject, message);
+		}
+	}
+}
+
